dc668e3a32df9f9f8c5ce23c4066bdca7a6eea55
Gaye <gayegul@gmail.com>
2016-07-07 15:57:02 -0700

    minor change

--- server.js
+++ server.js
@@ -11,18 +11,17 @@ app.get('/', function(req, res) {
 
 app.get('/searchrecipe/:q', function (req, res){
     var url = "https://api.projectoxford.ai/luis/v1/application?id=edab7fb9-9fb0-4940-8a2b-80e60a2b3abd&subscription-key=c4ac7ed5ec024d6584cdb0f4e4a97c56&q=";
-    var search = req.query.q;
-    url = utl + search;
+    var search = req.params.q;
+    console.log(req);
+    url = url + search;
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            res.send(body) // Show the HTML for the Google homepage. 
+            res.send(body) // Show the HTML for the Google homepage.
         }
     })
 
 
 });
-
-
 
 app.listen(PORT, function() {
   console.log('up and running');
